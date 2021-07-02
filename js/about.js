641dff055c1f86d478ac96240b8e79e092a71d46
AbhinavChowdaryM98 <abhinav.chowdary@students.iiit.ac.in>
2021-07-02 21:03:27 +0530

    Done With skill table and form using javascript

--- js/about.js
+++ js/about.js
@@ -1,9 +1,14 @@
-var arr = ["https://play-lh.googleusercontent.com/zZ0eKkHZQofg3YI7NpMy2CV6zHHO9HKmyXEvVh40PDVEcelqAzGVi0yr5CljPOhNILRS", "https://directory.edugorilla.com/wp-content/uploads/sites/6/2016/01/nnnn.jpg", "https://www.iiit.ac.in/img/iiit-new.png"]
+var arr = ["https://play-lh.googleusercontent.com/zZ0eKkHZQofg3YI7NpMy2CV6zHHO9HKmyXEvVh40PDVEcelqAzGVi0yr5CljPOhNILRS", 
+"https://directory.edugorilla.com/wp-content/uploads/sites/6/2016/01/nnnn.jpg", "https://www.iiit.ac.in/img/iiit-new.png",
+"../images/christian_bale.png", "../images/messi.jpg"]
 
 var a = 0
 
+var table = [];
+var table_len = 0;
+
 function changeimage() {
     a+=1;
-    a = a%3;
+    a = a%5;
     document.getElementById("education").src = arr[a];
 }
---
+++ js/form.js
@@ -0,0 +1,18 @@
+var arr = [];
+function myFunction() {
+    var name = document.getElementById("name").value;
+    document.getElementById("name").value = "";
+    var skill = document.getElementById("skill").value;
+    document.getElementById("skill").value = "";
+    var level = document.getElementById("level").value;
+    document.getElementById("level").value = "";
+    //  alert(temp)
+    var tab = document.getElementById("tab");
+    arr.push({ name, skill, level });
+    var htm = "";
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        htm = htm + `<tr><td>${element.name}</td><td>${element.skill}</td><td>${element.level}</td></tr>`;
+    }
+    tab.innerHTML = `<table><tr><th>Name</th><th>Skill</th><th>level</th></tr>${htm}</table>`;
+}
